3028e7bcb4db8925a953e8abf381159d9f219bac
Nikoswee <nikos-wee@hotmail.com>
2022-10-16 13:02:31 +0800

    updated with session

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, render_template, request, url_for, session, redirect
+from flask import Flask, jsonify, render_template, request, url_for, session, redirect, session
 import os
 from pymongo import MongoClient
 import db
 
 app = Flask(__name__)
+app.secret_key = "apple1234"
 
 # Connection to mongo database
 
@@ -17,6 +18,33 @@ def MongoDB():
 @app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        user = request.form["email"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template('login.html')
+
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}<h1>"
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
