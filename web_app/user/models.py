694405601bbafa7f11abdb563a3ec5ea184d8c7d
Nikoswee <nikos-wee@hotmail.com>
2022-10-23 17:22:57 +0800

    login auth and session done
    session
    hashed pw
    verification of password

--- web_app/user/models.py
+++ web_app/user/models.py
@@ -1,17 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect, url_for
 from passlib.hash import pbkdf2_sha256
 import uuid
+from db import db
+
+# status code 200 = OK request fulfilled
+# status code 400 = BAD request
+# status code 401 = Unauthorized entry
 
 
 class User:
+
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
     def signup(self):
-
         print(request.form)
-
         # create the user object
         user = {
-            "_id": uuid.uuid4(hex),
-            "name": request.form.get('username'),
+            "_id": uuid.uuid4().hex,
+            "username": request.form.get('username'),
             "email": request.form.get('email'),
             "password": request.form.get('password')
         }
@@ -19,4 +29,29 @@ class User:
         # encrypt the password
         user['password'] = pbkdf2_sha256.encrypt(user['password'])
 
-        return jsonify(user), 200
+        # check for existing email
+        if db.User.find_one({"email": user['email']}):
+            return jsonify({"error": "Email address already in use"}), 400
+
+        # insert user if there is no existing email
+        if db.User.insert_one(user):
+            return self.start_session(user)
+
+        # throw error
+        return jsonify({"error:" "Signup failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect(url_for("home"))
+
+    def login(self):
+
+        user = db.User.find_one({"email": request.form.get('email')})
+
+        # check for user email in db and if password matches
+        ## request.form.get('password') is un-encrypted
+        ## user['password'] is encrypted
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+
+        return jsonify({"error": "Invalid login credentials"}), 401
--- web_app/user/routes.py
+++ web_app/user/routes.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app_factory import app
+from app import app
 from user.models import User
 
 
-@app.route('/user/signup', methods=['POST'])
+@app.route('/user/signup/', methods=['POST'])
 def signup():
     return User().signup()
