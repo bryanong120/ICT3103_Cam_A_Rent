c907eee39b72497ddca64cd6af30418aadbecd94
Nikoswee <nikos-wee@hotmail.com>
2022-10-23 18:41:30 +0800

    small adjustments

--- web_app/user/models.py
+++ web_app/user/models.py
@@ -45,12 +45,11 @@ class User:
         return redirect(url_for("home"))
 
     def login(self):
-
         user = db.User.find_one({"email": request.form.get('email')})
-
         # check for user email in db and if password matches
         ## request.form.get('password') is un-encrypted
         ## user['password'] is encrypted
+
         if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
 
