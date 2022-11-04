3eec199bca6526b6be20281e723b482b622d18bb
bryanong120 <shadowfiend98@gmail.com>
2022-11-04 19:49:17 +0800

    update db.py

--- web_app/db.py
+++ web_app/db.py
@@ -1,5 +1,5 @@
 from flask_pymongo import pymongo
 
-DB_URI = "mongodb+srv://nikoswee:apple1234@rent-a-cam.tf5um47.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = "mongodb+srv://nikoswee:296JsRtDcmhJMD4a@rent-a-cam.tf5um47.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(DB_URI, tls=True)
 db = client.get_database('Cam-A-Rent')
